1d56bd53eff4d7e6ee65fed16af7b185f1ace034
Gerson Rocha <gersonrocha9@gmail.com>
2023-08-29 15:09:21 -0300

    🎨 add a folder structure

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
+      Register Page
+    </div>
+  )
+}
+
+export default RegisterPage
