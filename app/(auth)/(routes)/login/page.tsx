1e8a00aef4eaddbcb628c45eb56f6e7b18a1bf92
Gerson Rocha <gersonrocha9@gmail.com>
2023-08-29 15:31:19 -0300

    ✨ feat: implement authentication with clerk

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-      Login Page
-    </div>
-  )
-}
-
-export default LoginPage
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-      This is a protected page
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
